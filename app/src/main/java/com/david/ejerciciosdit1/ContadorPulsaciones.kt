ba3701c5183dc2aa5aa739f460d833a3865ef7bf
David Duque <davduquediaz@gmail.com>
2024-10-23 11:07:01 +0200

    mas ejercicios

--- app/src/main/java/com/david/ejerciciosdit1/ContadorPulsaciones.kt
+++ app/src/main/java/com/david/ejerciciosdit1/ContadorPulsaciones.kt
@@ -1,20 +1,30 @@
 package com.david.ejerciciosdit1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class ContadorPulsaciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.contador_pulsaciones)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        var boton=findViewById<Button>(R.id.boton)
+        var valor=0
+        var num=findViewById<TextView>(R.id.numero)
+        boton.setOnClickListener{
+            valor++
+            num.text=valor.toString()
         }
     }
 }
